fix f1 calculation

train.py
@@ -143,7 +143,7 @@ def train():
                     dev_summary_writer.add_summary(summaries, step)
 
                     time_str = datetime.datetime.now().isoformat()
-                    f1 = f1_score(np.argmax(y_dev, axis=1), predictions, labels=np.array(range(0, 9)), average="macro")
+                    f1 = f1_score(np.argmax(y_dev, axis=1), predictions, labels=np.array(range(0, 10)), average="macro")
                     print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
                     print("Macro-Average F1 Score: {:g}\n".format(f1))
 