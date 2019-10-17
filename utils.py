03c4a93074ec11d7d4bce43fc5ebc62fcef392fd
taufique74 <taufique74@hotmail.com>
2019-10-17 14:45:50 +0600

    modified labels for bangla

--- utils.py
+++ utils.py
@@ -1,27 +1,29 @@
 import numpy as np
 
 
-class2label = {'Other': 0,
-               'Message-Topic(e1,e2)': 1, 'Message-Topic(e2,e1)': 2,
-               'Product-Producer(e1,e2)': 3, 'Product-Producer(e2,e1)': 4,
-               'Instrument-Agency(e1,e2)': 5, 'Instrument-Agency(e2,e1)': 6,
-               'Entity-Destination(e1,e2)': 7, 'Entity-Destination(e2,e1)': 8,
-               'Cause-Effect(e1,e2)': 9, 'Cause-Effect(e2,e1)': 10,
-               'Component-Whole(e1,e2)': 11, 'Component-Whole(e2,e1)': 12,
-               'Entity-Origin(e1,e2)': 13, 'Entity-Origin(e2,e1)': 14,
-               'Member-Collection(e1,e2)': 15, 'Member-Collection(e2,e1)': 16,
-               'Content-Container(e1,e2)': 17, 'Content-Container(e2,e1)': 18}
+class2label = {'is_a(e1,e2)': 0,
+               'job_done(e1,e2)': 1, 
+               'at_location(e1,e2)': 2,
+               'has_a(e1,e2)': 3, 
+               'has_property(e1,e2)': 4,
+               'has_subevent(e1,e2)': 5, 
+               'results_in(e1,e2)': 6,
+               'used_for(e1,e2)': 7, 
+               'work_in(e1,e2)': 8,
+               'born_in(e1,e2)': 9
+               }
 
-label2class = {0: 'Other',
-               1: 'Message-Topic(e1,e2)', 2: 'Message-Topic(e2,e1)',
-               3: 'Product-Producer(e1,e2)', 4: 'Product-Producer(e2,e1)',
-               5: 'Instrument-Agency(e1,e2)', 6: 'Instrument-Agency(e2,e1)',
-               7: 'Entity-Destination(e1,e2)', 8: 'Entity-Destination(e2,e1)',
-               9: 'Cause-Effect(e1,e2)', 10: 'Cause-Effect(e2,e1)',
-               11: 'Component-Whole(e1,e2)', 12: 'Component-Whole(e2,e1)',
-               13: 'Entity-Origin(e1,e2)', 14: 'Entity-Origin(e2,e1)',
-               15: 'Member-Collection(e1,e2)', 16: 'Member-Collection(e2,e1)',
-               17: 'Content-Container(e1,e2)', 18: 'Content-Container(e2,e1)'}
+label2class = {0: 'is_a(e1,e2)',
+               1: 'job_done(e1,e2)', 
+               2: 'at_location(e1,e2)',
+               3: 'has_a(e1,e2)', 
+               4: 'has_property(e1,e2)',
+               5: 'has_subevent(e1,e2)', 
+               6: 'results_in(e1,e2)',
+               7: 'used_for(e1,e2)', 
+               8: 'work_in(e1,e2)',
+               9: 'born_in(e1,e2)'
+               }
 
 
 def load_glove(embedding_path, embedding_dim, vocab):
